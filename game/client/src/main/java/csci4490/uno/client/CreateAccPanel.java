206db31d30317a5dc8e29c55e6cfe084a36c058e
Whirvis Ardenaur <whirvis@ardenus.org>
2022-04-20 17:57:47 -0500

    Move code in unogame to game client module

---
+++ game/client/src/main/java/csci4490/uno/client/CreateAccPanel.java
@@ -0,0 +1,98 @@
+/*
+ * Kal Young
+ * Create Account Panel
+ */
+
+package csci4490.uno.client;
+
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.Set;
+
+import javax.imageio.ImageIO;
+import javax.swing.JTextField;
+import javax.swing.JEditorPane;
+
+public class CreateAccPanel extends JPanel
+{
+	private JTextField caccLabel;
+	private JLabel usrnmLabel;
+	private JLabel pswdLabel;
+	private JLabel verifyLabel;
+	private JEditorPane verifyPane;
+	private JEditorPane pswdPane;
+	private JEditorPane usrnmPane;
+	private JButton submitButton;
+	private JLabel errorLabel;
+	private JButton returnButton;
+	
+	public CreateAccPanel() {
+		setBackground(new Color(135, 206, 250));
+		setLayout(null);
+		
+		caccLabel = new JTextField();
+		caccLabel.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		caccLabel.setText("Enter a username and password below");
+		caccLabel.setBounds(101, 10, 245, 19);
+		add(caccLabel);
+		caccLabel.setColumns(10);
+		
+		usrnmLabel = new JLabel("Username:");
+		usrnmLabel.setFont(new Font("Tahoma", Font.BOLD, 16));
+		usrnmLabel.setBounds(31, 91, 99, 13);
+		add(usrnmLabel);
+		
+		pswdLabel = new JLabel("Password:");
+		pswdLabel.setFont(new Font("Tahoma", Font.BOLD, 16));
+		pswdLabel.setBounds(31, 125, 99, 13);
+		add(pswdLabel);
+		
+		verifyLabel = new JLabel("Verify Password:");
+		verifyLabel.setFont(new Font("Tahoma", Font.BOLD, 16));
+		verifyLabel.setBounds(31, 162, 144, 13);
+		add(verifyLabel);
+		
+		verifyPane = new JEditorPane();
+		verifyPane.setBounds(213, 156, 183, 19);
+		add(verifyPane);
+		
+		pswdPane = new JEditorPane();
+		pswdPane.setBounds(213, 125, 183, 19);
+		add(pswdPane);
+		
+		usrnmPane = new JEditorPane();
+		usrnmPane.setBounds(213, 91, 183, 19);
+		add(usrnmPane);
+		
+		submitButton = new JButton("Submit");
+		submitButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		submitButton.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		submitButton.setBounds(290, 247, 106, 43);
+		add(submitButton);
+		
+		errorLabel = new JLabel("  \r\n");
+		errorLabel.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		errorLabel.setBounds(31, 193, 365, 28);
+		add(errorLabel);
+		
+		returnButton = new JButton("Return");
+		returnButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		returnButton.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		returnButton.setBounds(31, 247, 120, 43);
+		add(returnButton);
+	}
+
+}
---
+++ game/client/src/main/java/csci4490/uno/client/ScoreBoardPanel.java
@@ -0,0 +1,115 @@
+/*
+ * Kal Young
+ * Score Board Panel
+ */
+
+package csci4490.uno.client;
+
+import javax.swing.JPanel;
+import java.awt.Color;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class ScoreBoardPanel extends JPanel
+{
+	
+	private JLabel winnerLabel;
+	private JLabel firstPlaceLabel;
+	private JLabel secondPlaceLabel;
+	private JLabel thirdPlaceLabel;
+	private JLabel fourthPlaceLabel;
+	private JLabel finumCardsLabel;
+	private JLabel firstNumCards;
+	private JLabel secNumCards;
+	private JLabel thirdNumCards;
+	private JLabel fourthNumCards;
+	private JButton playAgainButton;
+	private JButton exitGameButton;
+	
+	public ScoreBoardPanel() {
+		setBackground(new Color(0, 0, 0));
+		setForeground(new Color(255, 255, 255));
+		setLayout(null);
+		
+		winnerLabel = new JLabel("Player 1 Wins!");
+		winnerLabel.setFont(new Font("Comic Sans MS", Font.BOLD | Font.ITALIC, 18));
+		winnerLabel.setForeground(new Color(255, 255, 255));
+		winnerLabel.setBounds(178, 28, 137, 26);
+		add(winnerLabel);
+		
+		firstPlaceLabel = new JLabel("1st Place: Player 1");
+		firstPlaceLabel.setFont(new Font("Tahoma", Font.BOLD, 15));
+		firstPlaceLabel.setForeground(new Color(255, 255, 255));
+		firstPlaceLabel.setBounds(36, 104, 159, 26);
+		add(firstPlaceLabel);
+		
+		secondPlaceLabel = new JLabel("2nd Place: Player 2");
+		secondPlaceLabel.setForeground(new Color(255, 255, 255));
+		secondPlaceLabel.setFont(new Font("Tahoma", Font.BOLD, 15));
+		secondPlaceLabel.setBounds(36, 140, 159, 26);
+		add(secondPlaceLabel);
+		
+		thirdPlaceLabel = new JLabel("3rd Place: Player 3");
+		thirdPlaceLabel.setFont(new Font("Tahoma", Font.BOLD, 15));
+		thirdPlaceLabel.setForeground(new Color(255, 255, 255));
+		thirdPlaceLabel.setBounds(36, 176, 148, 19);
+		add(thirdPlaceLabel);
+		
+		fourthPlaceLabel = new JLabel("4th Place: Player 4");
+		fourthPlaceLabel.setFont(new Font("Tahoma", Font.BOLD, 15));
+		fourthPlaceLabel.setForeground(new Color(255, 255, 255));
+		fourthPlaceLabel.setBounds(36, 205, 147, 26);
+		add(fourthPlaceLabel);
+		
+		finumCardsLabel = new JLabel("Cards Left");
+		finumCardsLabel.setFont(new Font("Tahoma", Font.BOLD, 14));
+		finumCardsLabel.setForeground(new Color(255, 255, 255));
+		finumCardsLabel.setBounds(349, 76, 82, 13);
+		add(finumCardsLabel);
+		
+		firstNumCards = new JLabel("0");
+		firstNumCards.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		firstNumCards.setForeground(new Color(255, 255, 255));
+		firstNumCards.setBounds(377, 111, 45, 13);
+		add(firstNumCards);
+		
+		secNumCards = new JLabel("1");
+		secNumCards.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		secNumCards.setForeground(new Color(255, 255, 255));
+		secNumCards.setBounds(377, 148, 45, 13);
+		add(secNumCards);
+		
+		thirdNumCards = new JLabel("2");
+		thirdNumCards.setForeground(new Color(255, 255, 255));
+		thirdNumCards.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		thirdNumCards.setBounds(377, 181, 45, 13);
+		add(thirdNumCards);
+		
+		fourthNumCards = new JLabel("3");
+		fourthNumCards.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		fourthNumCards.setForeground(new Color(255, 255, 255));
+		fourthNumCards.setBounds(377, 214, 45, 13);
+		add(fourthNumCards);
+		
+		playAgainButton = new JButton("Play Again");
+		playAgainButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		playAgainButton.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		playAgainButton.setBounds(70, 272, 99, 31);
+		add(playAgainButton);
+		
+		exitGameButton = new JButton("Exit Game");
+		exitGameButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		exitGameButton.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		exitGameButton.setBounds(314, 272, 108, 31);
+		add(exitGameButton);
+	}
+}
